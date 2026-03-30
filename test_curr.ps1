9c1b315c5a6266d0b67cbeab7ec0692833348bc1
nirtunjay <nirtunjaykr@gmail.com>
2026-03-30 23:16:52 +0530

    1st

--- test_curr.ps1
+++
@@ -1,7 +0,0 @@
-$data = Invoke-RestMethod -Uri 'https://restcountries.com/v3.1/name/japan?fields=currencies'
-$curr = $data.currencies
-$keys = $curr.psobject.properties.value
-Write-Host "Keys count: $($keys.Count)"
-foreach ($prop in $curr.psobject.properties) {
-    Write-Host "Name: $($prop.Name), Value.name: $($prop.Value.name)"
-}
